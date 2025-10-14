7807f52bbfd4a981a77ea96f97a3fa50e31cc678
李诗军 <lidali@AllinoneBook.local>
2025-10-14 15:57:58 +0800

    2025年10月14日 星期二 15时57分58秒 CST

--- .make/depend.dir/libcpp.mk
+++ .make/depend.dir/libcpp.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libcpp.recursive
 ECHO_TARGETS += libcpp.echo
 libcpp.recursive:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/applications/mit/               >> /volumes/llama/home/alpine/libraries/libcpp/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/applications/mit/.make >> /volumes/llama/home/alpine/libraries/libcpp/.make/super
-	cd /volumes/llama/home/alpine/libraries/libcpp/ && make recursive && make install
-	-rm /volumes/llama/home/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/applications/mit/               >> /Users/lidali/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/applications/mit/.make >> /Users/lidali/alpine/libraries/libcpp/.make/super
+	cd /Users/lidali/alpine/libraries/libcpp/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make recursive && /Applications/Xcode.app/Contents/Developer/usr/bin/make install
+	-rm /Users/lidali/alpine/libraries/libcpp/.make/super
 libcpp.echo:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/applications/mit/               >> /volumes/llama/home/alpine/libraries/libcpp/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/applications/mit/.make >> /volumes/llama/home/alpine/libraries/libcpp/.make/super
-	cd /volumes/llama/home/alpine/libraries/libcpp/ && make echo
-	-rm /volumes/llama/home/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/applications/mit/               >> /Users/lidali/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/applications/mit/.make >> /Users/lidali/alpine/libraries/libcpp/.make/super
+	cd /Users/lidali/alpine/libraries/libcpp/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make echo
+	-rm /Users/lidali/alpine/libraries/libcpp/.make/super
--- .make/depend.dir/libmit.mk
+++ .make/depend.dir/libmit.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libmit.recursive
 ECHO_TARGETS += libmit.echo
 libmit.recursive:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/applications/mit/               >> /volumes/llama/home/alpine/libraries/libmit/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/applications/mit/.make >> /volumes/llama/home/alpine/libraries/libmit/.make/super
-	cd /volumes/llama/home/alpine/libraries/libmit/ && make recursive && make install
-	-rm /volumes/llama/home/alpine/libraries/libmit/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/applications/mit/               >> /Users/lidali/alpine/libraries/libmit/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/applications/mit/.make >> /Users/lidali/alpine/libraries/libmit/.make/super
+	cd /Users/lidali/alpine/libraries/libmit/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make recursive && /Applications/Xcode.app/Contents/Developer/usr/bin/make install
+	-rm /Users/lidali/alpine/libraries/libmit/.make/super
 libmit.echo:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/applications/mit/               >> /volumes/llama/home/alpine/libraries/libmit/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/applications/mit/.make >> /volumes/llama/home/alpine/libraries/libmit/.make/super
-	cd /volumes/llama/home/alpine/libraries/libmit/ && make echo
-	-rm /volumes/llama/home/alpine/libraries/libmit/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/applications/mit/               >> /Users/lidali/alpine/libraries/libmit/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/applications/mit/.make >> /Users/lidali/alpine/libraries/libmit/.make/super
+	cd /Users/lidali/alpine/libraries/libmit/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make echo
+	-rm /Users/lidali/alpine/libraries/libmit/.make/super
